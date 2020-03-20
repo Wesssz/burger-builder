abdebf185726e4b8d31bcc3983a399bb48fb8350
Wesssz <56195621+Wesssz@users.noreply.github.com>
2020-03-20 23:04:57 +0100

    Removed spread names from ingredients

--- src/Atoms/Spread.js
+++ src/Atoms/Spread.js
@@ -2,7 +2,7 @@ import React from "react";
 import style from "./Spread.module.css";
 
 const Spread = ({ spread }) => {
-  return <div className={style[spread]}>{spread}</div>;
+  return <div className={style[spread]}></div>;
 };
 
 export default Spread;
